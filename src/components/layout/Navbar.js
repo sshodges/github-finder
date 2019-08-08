405ebdda40617215f702da56b8538888d4714746
Samuel Hodges <samuel.hodges@4mations-MacBook-Pro.local>
2019-08-09 08:37:30 +1000

    Add users, API request and search funtionality

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,24 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-export class Navbar extends Component {
-  static defaultProps = {
-    title: 'Github Finder'
-  };
+const Navbar = ({ title }) => {
+  return (
+    <nav className='navbar bg-primary'>
+      <h1>
+        <i className='fab fa-github' /> {title}
+      </h1>
+    </nav>
+  );
+};
 
-  static propTypes = {
-    title: PropTypes.string.isRequired
-  };
+Navbar.defaultProps = {
+  title: 'Github Finder'
+};
 
-  render() {
-    return (
-      <nav className='navbar bg-primary'>
-        <h1>
-          <i className='fab fa-github' /> {this.props.title}
-        </h1>
-      </nav>
-    );
-  }
-}
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired
+};
 
 export default Navbar;
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,32 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export class UserItem extends Component {
-  state = {
-    id: 'id',
-    login: 'mojombo',
-    avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-    html_url: 'https://github.com/mojombo'
-  };
+const UserItem = ({ user: { login, id, avatar_url, html_url } }) => {
+  return (
+    <div className='card text-center'>
+      <img
+        src={avatar_url}
+        alt=''
+        className='round-img'
+        style={{ width: '60px' }}
+      />
 
-  render() {
-    const { login, id, avatar_url, html_url } = this.state;
-    return (
-      <div className='card text-center'>
-        <img
-          src={avatar_url}
-          alt=''
-          className='round-img'
-          style={{ width: '60px' }}
-        />
-
-        <div>
-          <a href={html_url} className='btn btn-dark btn-sm my-1'>
-            More Info
-          </a>
-        </div>
+      <div>
+        <a href={html_url} className='btn btn-dark btn-sm my-1'>
+          More Info
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired
+};
 
 export default UserItem;

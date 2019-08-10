0d2b295c91b853be618c079e7d88a0d9095b3538
sshodges <sshodges@gmail.com>
2019-08-10 11:16:03 +1000

    Add Router and About page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// Layouts
 import Navbar from "./components/layout/Navbar";
 import Alert from "./components/layout/Alert";
+// Users
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
+//Pages
+import About from "./components/pages/About";
 import "./App.css";
 import axios from "axios";
 
@@ -66,19 +71,32 @@ class App extends Component {
   render() {
     const { users, loading, alert } = this.state;
     return (
-      <div className='App'>
-        <Navbar title='Github Finder' />
-        <div className='container'>
-          <Alert alert={alert} />
-          <Search
-            searchUsers={this.searchUsers}
-            clearUsers={this.clearUsers}
-            showClear={users.length > 0 ? true : false}
-            setAlert={this.setAlert}
-          />
-          <Users loading={loading} users={users} />
+      <Router>
+        <div className='App'>
+          <Navbar title='Github Finder' />
+          <div className='container'>
+            <Alert alert={alert} />
+            <Switch>
+              <Route
+                exact
+                path='/'
+                render={props => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const Navbar = ({ title }) => {
   return (
@@ -7,12 +8,20 @@ const Navbar = ({ title }) => {
       <h1>
         <i className='fab fa-github' /> {title}
       </h1>
+      <ul>
+        <li>
+          <Link to=''>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
 
 Navbar.defaultProps = {
-  title: 'Github Finder'
+  title: "Github Finder"
 };
 
 Navbar.propTypes = {

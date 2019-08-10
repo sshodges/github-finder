2d863f0805d062a82be6d38b8128b552e2bc6882
sshodges <sshodges@gmail.com>
2019-08-10 10:57:02 +1000

    Add alert when no text entered

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
+import Alert from "./components/layout/Alert";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
 import "./App.css";
@@ -8,7 +9,8 @@ import axios from "axios";
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   };
 
   async componentDidMount() {
@@ -54,16 +56,25 @@ class App extends Component {
     });
   };
 
+  // Set Alert
+  setAlert = (message, type) => {
+    this.setState({ alert: { message, type } });
+
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  };
+
   render() {
-    const { users, loading } = this.state;
+    const { users, loading, alert } = this.state;
     return (
       <div className='App'>
         <Navbar title='Github Finder' />
         <div className='container'>
+          <Alert alert={alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
           <Users loading={loading} users={users} />
         </div>
